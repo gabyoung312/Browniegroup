6f976b5f53e0e1a7dba1549c97c100c474be9794
Patrick185748 <55426207+Patrick185748@users.noreply.github.com>
2019-11-01 21:15:41 +0800

    Update database.py

--- database.py
+++ database.py
@@ -4,7 +4,10 @@ products = {
      300: {"name":"Cappuccino","price":120},
      400: {"name":"Espresso","price":120},
      500: {"name":"Latte","price":140},
-     600: {"name":"Cold Brew","price":200}
+     600: {"name":"Cold Brew","price":200},
+     1000: {"name":"Tiramisu","price":150},
+     1100: {"name":"Red Velvet","price":130},
+     1200: {"name":"Tiramisu","price":150}
 }
 def get_product(code):
      return products[code]
